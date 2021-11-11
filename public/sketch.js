485fa60b8c384ef44ba08597e407f0bf7984c7ba
IronZack95 <zac.carrettoni@gmail.com>
2021-11-11 22:44:17 +0100

    Update sketch.js

--- public/sketch.js
+++ public/sketch.js
@@ -6,17 +6,7 @@ let multi
 // Inizio del programma
 lobby = new Lobby();
 
-
-/*
-function centerCanvas() {
-  var x = (windowWidth - width) / 2;
-  var y = (windowHeight - height) / 2;
-  cnv.position(x, y);
-}
-*/
-
-
-
+// eventi
 singleplayer.onclick = function(){
   lobby.getText().innerText = "Play!!";
   setTimeout(function(){ lobby.destructor(); delete pagina; single = new SinglePlayer();},2000)
