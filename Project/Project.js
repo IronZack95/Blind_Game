f1e4e93de7d9373a26fc7e3f6ae71ae9e1ca0845
Alessandro199762 <alessandro.zullo97@gmail.com>
2021-11-18 18:39:39 +0100

    update

--- Project/Project.js
+++ Project/Project.js
@@ -1,28 +1,47 @@
 function setup() {
   createCanvas(800, 600);
   background(153);
-  p = new Player(400,30);
+  p = new Player(width / 2, height / 10);
 }
-
 
 function draw() {
   background(153);
   p.update();
 }
 
-class Player{
 
-  constructor(x_start,y_start){
+
+
+
+
+class Player{ 
+  
+  
+  constructor(x_start, y_start){
+    this.v = createVector(width / 2, height / 2);
+    this.diameter = 20;
     this.x = x_start;
     this.y = y_start;
-    circle(this.x, this.y, 20);
+    circle(this.x, this.y, this.diameter);
+    triangle(this.x - this.diameter / 2, this.y, this.x + this.diameter / 2, this.y, this.x, this.y + this.diameter / 2);
   }
   
   update(){
     circle(this.x, this.y, 20);
+    triangle(this.x - this.diameter / 2, this.y, this.x + this.diameter / 2, this.y, mouseX, mouseY);
+    //triangle(this.x - this.diameter / 2, this.y, this.x + this.diameter / 2, this.y, this.x, this.y + this.diameter / 2);
   }
- 
+   
+   
+  updateDirection(){
+    
+  } 
 }
+
+
+
+
+
 
 function keyPressed() {
         if (key == 'a') {
