b71b72b9dc7b1e45eb8290687171a58144d7a00a
IronZack95 <zac.carrettoni@gmail.com>
2021-11-25 02:01:42 +0100

    Update PlayerDesign.js

--- ConceptArt/PlayerDesign/PlayerDesign.js
+++ ConceptArt/PlayerDesign/PlayerDesign.js
@@ -1,30 +1,41 @@
+let i = 0;
+let walk = false;
+
 function setup() {
   createCanvas(400, 400);
   background(220);
-  p = new Players(width/2,height/2);
+  p = new Players(width/2,height/2);  
 }
 
 function draw() {
   clear();
   
+  if(i>frameRate()){ i=0;}
+  else{i++;}
+  
+  walk = false;
    let dir = (2*PI*winMouseX/windowWidth);  //tra 0 e 1
   if(dir <= 2*PI && dir > 0){p.dir = dir;}
   
   background(220);
   if (keyIsDown(LEFT_ARROW)) {
     p.x -= 1;
+    walk = true;
   }
 
   if (keyIsDown(RIGHT_ARROW)) {
     p.x += 1;
+    walk = true;
   }
 
   if (keyIsDown(UP_ARROW)) {
     p.y -= 1;
+    walk = true;
   }
 
   if (keyIsDown(DOWN_ARROW)) {
     p.y += 1;
+    walk = true;
   }
   p.update();
 }
@@ -48,9 +59,23 @@ class Players{
     rect(this.xBody, this.yBody, this.w, this.w-this.w/7,this.radius);
     // testa
     circle(this.xHead, this.yHead,this.w);
-    // gambe
-    rect(this.xBody, this.yBody+this.w/2, this.w/4, this.w/1.5,this.radius);
-    rect(this.xBody+this.w-this.w/4, this.yBody+this.w/2, this.w/4, this.w/1.5,this.radius);
+    
+    // gambe partenza // walk cycle
+    if( (i >= 0 && i< frameRate()/4) || (i > 2*frameRate()/4 && i< 3*frameRate()/4) || (walk == false)){
+      //console.log('0')
+      rect(this.xBody, this.yBody+this.w/2, this.w/4, this.w/1.5,this.radius);
+      rect(this.xBody+this.w-this.w/4, this.yBody+this.w/2, this.w/4, this.w/1.5,this.radius);
+    }else if( i >=frameRate()/4 && i<= 2*frameRate()/4){
+      //console.log('-1')
+      rect(this.xBody, this.yBody+this.w/2, this.w/4, this.w/1.5-this.w/8,this.radius);
+      rect(this.xBody+this.w-this.w/4, this.yBody+this.w/2, this.w/4, this.w/1.5,this.radius);
+    }else if(i>=3*frameRate()/4){
+      //console.log('1')
+      rect(this.xBody, this.yBody+this.w/2, this.w/4, this.w/1.5,this.radius);
+      rect(this.xBody+this.w-this.w/4, this.yBody+this.w/2, this.w/4, this.w/1.5-this.w/8,this.radius);
+    }
+    // walk cycle
+    
     //fill(255,255,255);
  
     //circle(this.xHead-this.w/6, this.yHead-this.w/5,this.w/3);
